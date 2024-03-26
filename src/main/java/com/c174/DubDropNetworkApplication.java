9b08e91ec695a00b0b68dce621be3a350efe53bd
Matias <akucfh@gmail.com>
2024-03-26 13:48:34 +0100

    first commit

---
+++ src/main/java/com/c174/DubDropNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.c174;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubDropNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubDropNetworkApplication.class, args);
+	}
+
+}
