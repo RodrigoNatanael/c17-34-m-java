00359db8fc5174841f5e0640026fe842aca96ee2
Matias <akucfh@gmail.com>
2024-03-28 13:35:12 +0100

    first commit

---
+++ src/test/java/com/c174/EnterpriceCheckInSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c174;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriceCheckInSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
