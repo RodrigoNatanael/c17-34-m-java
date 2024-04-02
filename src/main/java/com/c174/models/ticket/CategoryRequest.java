803bfb9cb7b255d8423ce4e69c2439990cafeae6
Nesarifr <93294641+Nesarifr@users.noreply.github.com>
2024-04-02 19:06:32 -0300

    [merge entites]

--- src/main/java/com/c174/models/ticket/CategoryRequest.java
+++ src/main/java/com/c174/models/ticket/CategoryRequest.java
@@ -1,11 +1,13 @@
 package com.c174.models.ticket;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class CategoryRequest {
+    @Pattern(regexp = "^[a-zA-Z0-9 ]{1,50}$", message = "Invalid category name")
     private String name;
     public CategoryRequest(String name) {
         this.name = name;
